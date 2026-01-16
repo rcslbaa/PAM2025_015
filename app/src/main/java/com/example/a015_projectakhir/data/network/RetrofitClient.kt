48cbf7cb97d672313a0e4dd52ab853350b1b507a
rcslbaa <ifasmarani3125@gmail.com>
2026-01-17 01:36:20 +0700

    menambahkan retrofitclient

---
+++ app/src/main/java/com/example/a015_projectakhir/data/network/RetrofitClient.kt
@@ -0,0 +1,23 @@
+package com.example.a015_projectakhir.data.network
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://172.26.128.1:3000/"
+
+    val instance: ApiService by lazy {
+        // Menambahkan Gson Lenient untuk mengatasi malformed JSON
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        retrofit.create(ApiService::class.java)
+    }
+}
