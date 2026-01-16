c8bec16d1da6c7d3a6c803e24a2b3677874fb7e2
rcslbaa <ifasmarani3125@gmail.com>
2026-01-17 01:28:24 +0700

    menambahkan apiservice

---
+++ app/src/main/java/com/example/a015_projectakhir/data/network/ApiService.kt
@@ -0,0 +1,46 @@
+package com.example.a015_projectakhir.data.network
+
+import com.example.a015_projectakhir.data.model.GenericResponse
+import com.example.a015_projectakhir.data.model.Layanan
+import com.example.a015_projectakhir.data.model.Pesanan
+import com.example.a015_projectakhir.data.model.User
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.HTTP
+import retrofit2.http.POST
+
+interface ApiService {
+
+    // 1. AUTHENTICATION
+    @POST("login")
+    suspend fun login(@Body loginData: Map<String, String>): LoginResponse
+
+    // 2. MASTER LAYANAN (ADMIN)
+    @GET("get_layanan")
+    suspend fun getLayanan(): List<Layanan>
+
+    @POST("add_layanan")
+    suspend fun addLayanan(@Body data: Map<String, Any>): GenericResponse
+
+    @POST("update_layanan")
+    suspend fun updateLayanan(@Body data: Map<String, Any>): GenericResponse
+
+    @HTTP(method = "DELETE", path = "delete_layanan", hasBody = true)
+    suspend fun deleteLayanan(@Body data: Map<String, Int>)
+
+    // 3. TRANSAKSI & PESANAN (STAFF)
+    @GET("get_pesanan")
+    suspend fun getPesanan(): List<Pesanan>
+
+    @POST("add_pesanan")
+    suspend fun addPesanan(@Body data: Map<String, Any>): GenericResponse
+
+    @POST("update_status_pesanan")
+    suspend fun updateStatus(@Body data: Map<String, Any>): GenericResponse
+}
+
+data class LoginResponse(
+    val status: String,
+    val message: String,
+    val data: User?
+)
