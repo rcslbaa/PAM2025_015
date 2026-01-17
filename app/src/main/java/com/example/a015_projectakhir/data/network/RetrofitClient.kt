d1fa85040ed43f7570de381e88705152e66b0179
rcslbaa <ifasmarani3125@gmail.com>
2026-01-17 17:07:20 +0700

    update retofit client

--- app/src/main/java/com/example/a015_projectakhir/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/a015_projectakhir/data/network/RetrofitClient.kt
@@ -1,20 +1,32 @@
 package com.example.a015_projectakhir.data.network
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://172.26.128.1:3000/"
+    // CATATAN: Pastikan IP ini SAMA dengan IPv4 di CMD (ipconfig)
+    private const val BASE_URL = "http://10.0.2.2:3000/"
 
     val instance: ApiService by lazy {
-        // Menambahkan Gson Lenient untuk mengatasi malformed JSON
-        val gson = GsonBuilder()
-            .setLenient()
-            .create()
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .connectTimeout(15, TimeUnit.SECONDS) // Menambah waktu tunggu koneksi
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+
+        val gson = GsonBuilder().setLenient().create()
 
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
